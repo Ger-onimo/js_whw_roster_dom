df613570fdd5ef4606887cf2fb3f8062a292473d
Gerry Waterston <gwaterston@hotmail.co.uk>
2019-01-13 20:03:56 +0000

    Added radio buttons

--- js/app.js
+++ js/app.js
@@ -2,11 +2,14 @@ document.addEventListener('DOMContentLoaded', () => {
   // console.log('JavaScript loaded');
   const formSubmit = document.querySelector('#new-item-form');
   formSubmit.addEventListener('submit', handleFormSubmit);
+
+  const deleteAllClick = document.querySelector('#delete-all');
+  deleteAllClick.addEventListener('click', handleDeleteAllClick)
 });
 
 // //////HANDLER - form
   const handleFormSubmit = function (event) {
-  // console.log(event);
+  console.log(event);
   event.preventDefault();
   const memberProfileItem = createMemberProfileItem(event.target);
   const memberProfile = document.querySelector('#member-profiles');
@@ -16,25 +19,50 @@ document.addEventListener('DOMContentLoaded', () => {
 };
 
 
-// ///////// - create to elements of the form
+// ///////// - create the elements of the form
   const createMemberProfileItem = function (form) {
-    const memberProfileItem = document.createElement('li');
-    memberProfileItem.classList.add('member-profile-item');
+    console.log(form)
+    const memberProfileItem = document.createElement('ul');
+    // memberProfileItem.classList.add('member-profile-item'); // only for class
 
-// define derby name
-    const name = document.createElement('h2');
-    name.textContent = "Derby Name: " + form.name.value;
-    memberProfileItem.appendChild(name);
+    // TODO - nest a memberProfileItemDetail inside memberProfileItem
 
-// define position
-    const position = document.createElement('h3');
-    position.textContent = "Position: " + form.position.value;
-    memberProfileItem.appendChild(position);
+/// addition to plan - create memberProfileItemDetail element
+// const createMemberProfileItemDetail = function ...
+//   const memberProfileItemDetail = document.createElement('li');
+//   memberProfileItemDetail.classList.add('member-profile-item-detail');
 
-// define team
-    const team = document.createElement('h3');
-    team.textContent = "Team: " + form.team.value;
-    memberProfileItem.appendChild(team);
+        // define derby name
+            const name = document.createElement('h3');
+            name.textContent = "Derby Name: " + form.name.value;
+            memberProfileItem.appendChild(name); // change to memberProfileItemDetail
+
+        // define position
+            const position = document.createElement('h4');
+            position.textContent = "Position: " + form.position.value;
+            memberProfileItem.appendChild(position);
+
+        // define team
+            const team = document.createElement('h4');
+            team.textContent = "Team: " + form.team.value;
+            memberProfileItem.appendChild(team);
+
+        // addition to plan
+        // define pronoun
+            const pronoun = document.createElement('p');
+            pronoun.textContent = "Preferred pronoun: " + form.pronoun.value;
+            memberProfileItem.appendChild(pronoun);
+
+        // // define start date
+        //     const pronoun = document.createElement('p');
+        //     startDate.textContent = "Start Date: " + form.startDate.value;
+        //     memberProfileItem.appendChild(startDate);
 
   return memberProfileItem;
 };
+
+  const handleDeleteAllClick = function (event) {
+    console.log(event);
+    const memberProfile = document.querySelector('#member-profiles');
+    memberProfile.innerHTML = '';
+  }
